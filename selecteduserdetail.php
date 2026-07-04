95f7c65dcc8cdd914c4ee0a2e667d239caab2126
Digvijaysing Rajput <geniusdigvijaysing@gmail.com>
2026-07-04 17:16:38 +0000

    Secure transfers with prepared statements and transactions

--- selecteduserdetail.php
+++ selecteduserdetail.php
@@ -3,79 +3,90 @@ include 'config.php';
 
 if(isset($_POST['submit']))
 {
-    $from = $_GET['id'];
-    $to = $_POST['to'];
-    $amount = $_POST['amount'];
+    $from = filter_var($_GET['id'], FILTER_VALIDATE_INT);
+    $to = filter_var($_POST['to'], FILTER_VALIDATE_INT);
+    $amount = filter_var($_POST['amount'], FILTER_VALIDATE_FLOAT);
 
-    $sql = "SELECT * from users where id=$from";
-    $query = mysqli_query($conn,$sql);
-    $sql1 = mysqli_fetch_array($query); // returns array or output of user from which the amount is to be transferred.
-
-    $sql = "SELECT * from users where id=$to";
-    $query = mysqli_query($conn,$sql);
-    $sql2 = mysqli_fetch_array($query);
-
-
-
-    // constraint to check input of negative value by user
-    if (($amount)<0)
-   {
+    if ($from === false || $to === false || $amount === false) {
+        echo "<script>alert('Invalid transfer details');</script>";
+    }
+    else if ($from === $to) {
+        echo "<script>alert('Sender and receiver must be different');</script>";
+    }
+    else if ($amount < 0) {
         echo '<script type="text/javascript">';
         echo ' alert("Oops! Negative values cannot be transferred")';
         echo '</script>';
     }
-
-
-  
-    // constraint to check insufficient balance.
-    else if($amount > $sql1['balance']) 
-    {
-        
-        echo '<script type="text/javascript">';
-        echo ' alert("Bad Luck! Insufficient Balance")';
-        echo '</script>';
-    }
-    
-
-
-    // constraint to check zero values
-    else if($amount == 0){
-
+    else if ($amount == 0) {
          echo "<script type='text/javascript'>";
          echo "alert('Oops! Zero value cannot be transferred')";
          echo "</script>";
      }
-
-
     else {
-        
-                // deducting amount from sender's account
-                $newbalance = $sql1['balance'] - $amount;
-                $sql = "UPDATE users set balance=$newbalance where id=$from";
-                mysqli_query($conn,$sql);
-             
+        $senderStmt = mysqli_prepare($conn, "SELECT id, name, balance FROM users WHERE id = ?");
+        mysqli_stmt_bind_param($senderStmt, "i", $from);
+        mysqli_stmt_execute($senderStmt);
+        $senderResult = mysqli_stmt_get_result($senderStmt);
+        $sender = mysqli_fetch_assoc($senderResult);
+        mysqli_stmt_close($senderStmt);
 
-                // adding amount to reciever's account
-                $newbalance = $sql2['balance'] + $amount;
-                $sql = "UPDATE users set balance=$newbalance where id=$to";
-                mysqli_query($conn,$sql);
-                
-                $sender = $sql1['name'];
-                $receiver = $sql2['name'];
-                $sql = "INSERT INTO transaction(`sender`, `receiver`, `balance`) VALUES ('$sender','$receiver','$amount')";
-                $query=mysqli_query($conn,$sql);
+        $receiverStmt = mysqli_prepare($conn, "SELECT id, name, balance FROM users WHERE id = ?");
+        mysqli_stmt_bind_param($receiverStmt, "i", $to);
+        mysqli_stmt_execute($receiverStmt);
+        $receiverResult = mysqli_stmt_get_result($receiverStmt);
+        $receiver = mysqli_fetch_assoc($receiverResult);
+        mysqli_stmt_close($receiverStmt);
 
-                if($query){
-                     echo "<script> alert('Transaction Successful');
-                                     window.location='transactionhistory.php';
-                           </script>";
-                    
-                }
-
-                $newbalance= 0;
-                $amount =0;
+        if (!$sender || !$receiver) {
+            echo "<script>alert('Invalid sender or receiver account');</script>";
         }
-    
+        else if ($amount > $sender['balance']) {
+            echo '<script type="text/javascript">';
+            echo ' alert("Bad Luck! Insufficient Balance")';
+            echo '</script>';
+        }
+        else {
+            mysqli_begin_transaction($conn);
+
+            $senderBalance = $sender['balance'] - $amount;
+            $receiverBalance = $receiver['balance'] + $amount;
+
+            $debitStmt = mysqli_prepare($conn, "UPDATE users SET balance = ? WHERE id = ?");
+            mysqli_stmt_bind_param($debitStmt, "di", $senderBalance, $from);
+            $debitOk = mysqli_stmt_execute($debitStmt);
+            mysqli_stmt_close($debitStmt);
+
+            $creditStmt = mysqli_prepare($conn, "UPDATE users SET balance = ? WHERE id = ?");
+            mysqli_stmt_bind_param($creditStmt, "di", $receiverBalance, $to);
+            $creditOk = mysqli_stmt_execute($creditStmt);
+            mysqli_stmt_close($creditStmt);
+
+            $transactionStmt = mysqli_prepare(
+                $conn,
+                "INSERT INTO transaction(sender, receiver, balance) VALUES (?, ?, ?)"
+            );
+            mysqli_stmt_bind_param(
+                $transactionStmt,
+                "ssd",
+                $sender['name'],
+                $receiver['name'],
+                $amount
+            );
+            $transactionOk = mysqli_stmt_execute($transactionStmt);
+            mysqli_stmt_close($transactionStmt);
+
+            if ($debitOk && $creditOk && $transactionOk) {
+                mysqli_commit($conn);
+                echo "<script> alert('Transaction Successful');
+                                window.location='transactionhistory.php';
+                      </script>";
+            } else {
+                mysqli_rollback($conn);
+                echo "<script>alert('Transaction failed. Please try again.');</script>";
+            }
+        }
+    }
 }
 ?>
 
@@ -113,15 +124,18 @@ if(isset($_POST['submit']))
 	<div class="container">
         <h2 class="text-center pt-4" style="color : black;">Transaction</h2>
             <?php
-                include 'config.php';
-                $sid=$_GET['id'];
-                $sql = "SELECT * FROM  users where id=$sid";
-                $result=mysqli_query($conn,$sql);
-                if(!$result)
+                $sid = filter_var($_GET['id'], FILTER_VALIDATE_INT);
+                $userStmt = mysqli_prepare($conn, "SELECT id, name, email, balance FROM users WHERE id = ?");
+                mysqli_stmt_bind_param($userStmt, "i", $sid);
+                mysqli_stmt_execute($userStmt);
+                $result = mysqli_stmt_get_result($userStmt);
+                $rows = mysqli_fetch_assoc($result);
+                mysqli_stmt_close($userStmt);
+
+                if(!$rows)
                 {
-                    echo "Error : ".$sql."<br>".mysqli_error($conn);
-                }
-                $rows=mysqli_fetch_assoc($result);
+                    echo "Error: user not found";
+                } else {
             ?>
             <form method="post" name="tcredit" class="tabletext" ><br>
         <div>
@@ -133,10 +147,10 @@ if(isset($_POST['submit']))
                     <th class="text-center">Balance</th>
                 </tr>
                 <tr style="color : black;">
-                    <td class="py-2"><?php echo $rows['id'] ?></td>
-                    <td class="py-2"><?php echo $rows['name'] ?></td>
-                    <td class="py-2"><?php echo $rows['email'] ?></td>
-                    <td class="py-2"><?php echo $rows['balance'] ?></td>
+                    <td class="py-2"><?php echo htmlspecialchars($rows['id']); ?></td>
+                    <td class="py-2"><?php echo htmlspecialchars($rows['name']); ?></td>
+                    <td class="py-2"><?php echo htmlspecialchars($rows['email']); ?></td>
+                    <td class="py-2"><?php echo htmlspecialchars($rows['balance']); ?></td>
                 </tr>
             </table>
         </div>
@@ -145,24 +159,25 @@ if(isset($_POST['submit']))
         <select name="to" class="form-control" required>
             <option value="" disabled selected>Choose</option>
             <?php
-                include 'config.php';
-                $sid=$_GET['id'];
-                $sql = "SELECT * FROM users where id!=$sid";
-                $result=mysqli_query($conn,$sql);
+                $listStmt = mysqli_prepare($conn, "SELECT id, name, balance FROM users WHERE id != ?");
+                mysqli_stmt_bind_param($listStmt, "i", $sid);
+                mysqli_stmt_execute($listStmt);
+                $result = mysqli_stmt_get_result($listStmt);
                 if(!$result)
                 {
-                    echo "Error ".$sql."<br>".mysqli_error($conn);
+                    echo "Error loading users";
                 }
-                while($rows = mysqli_fetch_assoc($result)) {
+                while($recipient = mysqli_fetch_assoc($result)) {
             ?>
-                <option class="table" value="<?php echo $rows['id'];?>" >
+                <option class="table" value="<?php echo htmlspecialchars($recipient['id']);?>" >
                 
-                    <?php echo $rows['name'] ;?> (Balance: 
-                    <?php echo $rows['balance'] ;?> ) 
+                    <?php echo htmlspecialchars($recipient['name']); ?> (Balance: 
+                    <?php echo htmlspecialchars($recipient['balance']); ?> ) 
                
                 </option>
             <?php 
-                } 
+                }
+                mysqli_stmt_close($listStmt);
             ?>
             <div>
         </select>
@@ -175,6 +190,7 @@ if(isset($_POST['submit']))
             <button class="btn mt-3" name="submit" type="submit" id="myBtn" >Transfer</button>
             </div>
         </form>
+            <?php } ?>
     </div>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
